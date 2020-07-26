12abb1bef14b98ae609c8f3ec450fdf60819a975
taylorhl <hbrecht2@live.maryville.edu>
2020-07-26 16:37:43 -0400

    Added editing option to items

--- groceryList/src/app/tab1/tab1.page.ts
+++ groceryList/src/app/tab1/tab1.page.ts
@@ -42,6 +42,16 @@ export class Tab1Page {
     this.items.splice(index, 1)
   }
 
+  async editItem(item, index){
+    console.log("Edit Item - ", item, index)
+    const toast = await this.toastController.create({
+      message: 'Editing ' + item.name + ' from grocery list...',
+      duration: 2000
+    });
+    toast.present();
+    this.presentEditPrompt(item, index);
+  }
+
   addItem(){
     console.log("Item added.");
     this.presentAlertPrompt();
@@ -77,6 +87,46 @@ export class Tab1Page {
           handler: (item) => {
             console.log('Confirm Ok', item);
             this.items.push(item);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentEditPrompt(item, index) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Edit Item',
+      message: 'Please edit item below.',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Item Name',
+          value: item.name
+        },
+        {
+          name: 'quantity',
+          type: 'text',
+          placeholder: 'Item Quantity',
+          value: item.quantity
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Save',
+          handler: (item) => {
+            console.log('Confirm Ok', item);
+            this.items[index] = item
           }
         }
       ]
